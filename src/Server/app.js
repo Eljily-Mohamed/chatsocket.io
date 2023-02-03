85bb619765b9344b19f35630a2fd166ddc41871c
hamade-kj <hamadekj15@gmail.com>
2023-02-03 11:12:44 +0100

    get update

--- src/Server/app.js
+++ src/Server/app.js
@@ -1,1 +1,10 @@
-const app = require('express');
+
+const express = require('express');
+//declare app qui prend fonction express 
+const app = express();
+//decalre make notre Server lisn in Port 1234 pour any Connexion 
+//si il ya une demande deconnexion on redirecte vers notre Statci file 
+app.listen(1234,function(){
+       app.use(express.static("./App.js"));
+})
+//on faire setup de notre Socket
