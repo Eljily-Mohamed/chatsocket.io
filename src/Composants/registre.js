10dee056da037179cb82893ed93e367c1afad88f
hamade-kj <hamadekj15@gmail.com>
2023-02-03 01:24:31 +0100

    get update

---
+++ src/Composants/registre.js
@@ -0,0 +1,17 @@
+
+const Registre = function () {
+    return (
+        <div className="Registre-div">
+            <div className="Registre-child">
+                 <div className="Registre-input">
+                       <input type="text" className="name-input" placeholder="Your Name"></input>
+                 </div>
+                 <div className="Registre-button">
+                       <button className="button-registre">Login</button>
+                 </div>
+            </div>
+        </div>
+    );
+}
+
+export default Registre;
