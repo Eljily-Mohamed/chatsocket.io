870914061afdc6544d41d3ae8fdd9492f2827df6
Eljily-Mohamed <hamadekj15@gmail.com>
2023-04-16 01:36:33 +0000

    get update

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,55 +1,87 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { BsMoon, BsSun } from "react-icons/bs";
+import { Link } from "react-router-dom";
 const Join = () => {
   const [name, setName] = useState("");
   const [roomName, setRoomName] = useState("");
-  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "white");
 
   const changeTheme = () => {
-    if (theme === "light") {
-      setTheme("dark");
+    if (theme === "white") {
+      setTheme("black");
     } else {
-      setTheme("light");
+      setTheme("white");
     }
   };
   useEffect(() => {
     localStorage.setItem("theme", theme);
-    document.body.className = theme;
+    document.body.style.backgroundColor = theme;
   }, [theme]);
 
   return (
-    <div className="join">
-      <div className="w-[100%] h-[200px] p-[20px] flex justify-between">
+    <div className={`${theme}`}>
+      <div className="w-[100%] h-[200px] lg:p-[100px] p-[20px] flex justify-between">
         <img
           src="https://mailmeteor.com/logos/assets/PNG/Google_Chat_Logo_512px.png"
           alt="logo"
           className="w-[100px] h-[100px] "
         />
-        <BsMoon
-          className="text-[30px] self-center text-white"
-          onClick={changeTheme}
-        />
+        {theme === "white" ? (
+          <BsMoon className="text-[30px] mt-[30px]" onClick={changeTheme} />
+        ) : (
+          <BsSun
+            className="text-[30px] mt-[30px] text-white"
+            onClick={changeTheme}
+          />
+        )}
       </div>
       <div className="w-[100%] m-auto h-auto p-[20px] lg:mt-[200px] mt-[10px] ">
-        <div className="w-[400px] m-auto">
-          <div className="lg:w-[500px] w-[350px] h-[50px] py-[10px] border-b-[2px] border-black">
-            <p className="text-center font-bold text-[20px]">Join</p>
+        <div className="w-[400px] lg:w-[500px] m-auto">
+          <div
+            className={`lg:w-[500px] w-[350px] h-[50px] py-[10px] border-b-[2px] border-${
+              theme == "white" ? "black" : "white"
+            }`}
+          >
+            <p
+              className={`text-center font-bold text-[20px] text-${
+                theme == "white" ? "black" : "white"
+              }`}
+            >
+              Join
+            </p>
           </div>
           <input
             type="text"
             placeholder="Your Name"
-            className="flex lg:w-[500px] w-[350px] h-[50px] py-[10px] px-[5px] rounded-[3px] outline-none bg-black mt-[20px] text-white"
+            className={`flex lg:w-[500px] w-[350px] h-[50px] py-[10px] px-[5px] rounded-[3px] outline-none bg-${
+              theme == "white" ? "black" : "white"
+            } mt-[20px] `}
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
           />
           <input
             type="text"
             placeholder="Room Name"
-            className="flex lg:w-[500px] w-[350px] h-[50px] py-[10px] px-[5px] rounded-[3px] outline-none mt-[20px] bg-black text-white"
+            className={`flex lg:w-[500px] w-[350px] h-[50px] py-[10px] px-[5px] rounded-[3px] outline-none bg-${
+              theme == "white" ? "black" : "white"
+            } mt-[20px] `}
+            onChange={(event) => {
+              setRoomName = event.target.value;
+            }}
           />
-          <div className="w-[250px] lg:w-[100px] m-auto ">
-            <button className="w-[200px] mt-[20px] h-[50px] bg-[#000] text-white rounded-[5px] border-none p-[15px] ">
-              Join Room
-            </button>
+          <div className="w-[250px] lg:w-[180px] m-auto ">
+            <Link to={`/chat?${name}&${roomName}${theme}`}>
+              <button
+                className={`w-[200px] mt-[20px] h-[50px] font-bold bg-red-500 rounded-[5px] border-none p-[15px] text-${
+                  theme == "white" ? "black" : "white"
+                }`}
+                type="submit"
+              >
+                Join Room
+              </button>
+            </Link>
           </div>
         </div>
       </div>
