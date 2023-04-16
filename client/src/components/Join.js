9abc32a224df2a70b88a585f7dc940e7a30adda2
Eljily-Mohamed <hamadekj15@gmail.com>
2023-04-16 00:02:53 +0000

    get update

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { useState } from "react";
 
 const Join = () => {
-  return <h2 className="">Hello in Join </h2>;
+  const [name, setName] = useState("");
+  const [roomName, setRoomName] = useState("");
+  return (
+    <div className="join">
+       <div className="w-[100%] bg-black">
+         <img src="https://logowik.com/content/uploads/images/google-chat4663.jpg" alt="logo"/>
+       </div>
+    </div>
+  );
 };
 
 export default Join;
