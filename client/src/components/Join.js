688febd0ce73b80b2df1483d3173b89ca6bf8bd1
Eljily-Mohamed <hamadekj15@gmail.com>
2023-04-16 00:30:12 +0000

    get update

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -6,8 +6,21 @@ const Join = () => {
   const [roomName, setRoomName] = useState("");
   return (
     <div className="join">
-       <div className="w-[100%] bg-black">
-         <img src="https://logowik.com/content/uploads/images/google-chat4663.jpg" alt="logo"/>
+      <div className="w-[100%] h-[200px] p-[20px]">
+        <img
+          src="https://logowik.com/content/uploads/images/google-chat4663.jpg"
+          alt="logo"
+          className="w-[100px] h-[100px]"
+        />
+      </div>
+       <div className="w-[100%] m-auto h-auto lg:mt-[200px] mt-[100px] ">
+          <div className="w-[400px] m-auto">
+             <div className="title">
+               <p>Join</p>
+             </div>
+             <input type="text" placeholder="Your Name" className="flex w-[500px] h-[50px] py-[10px] rounded-[3px] outline-none bg-black "/>
+             <input type="text" placeholder="Room Name" className="flex w-[500px] h-[50px] py-[10px] rounded-[3px] outline-none mt-[20px] bg-black"/>
+          </div>
        </div>
     </div>
   );
