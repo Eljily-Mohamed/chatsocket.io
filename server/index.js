bf6f6139faa5cf0a16af992a2ce9822d1e035b0c
Eljily-Mohamed <hamadekj15@gmail.com>
2023-04-17 08:27:12 +0000

    get update

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const socketIo = require("socket.io");
 const cors = require("cors");
 const http = require("http");
-
+const { getAllUsers, addUser, removeUser, getUser } = require("./users");
 // create notre server
 const PORT = process.env.PORT || 5000;
 // import router here
@@ -21,16 +21,28 @@ app.use(cors());
 app.use(router);
 
 io.on("connect", (socket) => {
-  console.log("connexion established")
-  
-  socket.on("join", ({ nameUser, roomName }) => {
-    console.log(nameUser, roomName);
+  console.log("connexion established");
+
+  socket.on("join", ({ nameUser, roomName }, callBack) => {
+    const { error, user } = addUser({ id: socket.id, nameUser, roomName });
+    if (error) {
+      return callBack(error);
+    }
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.nameUser} welcome to the room ${user.roomName}`,
+    });
+    socket.broadcast.to(user.roomName).emit("message", {
+      user: "admin",
+      text: `${user.nameUser} has joined the room`,
+    });
+    socket.join(user.roomName);
+    callBack();
   });
 
   socket.on("disconnect", () => {
     console.log("connection closed");
   });
-
 });
 
 server.listen(PORT, () => console.log(`Server listening on ${PORT}`));
---
+++ server/users.js
@@ -0,0 +1,38 @@
+let users = [];
+
+const addUser = ({ id, nameUser, roomName }) => {
+  nameUser = nameUser.trim().toLowerCase();
+  roomName = roomName.trim().toLowerCase();
+  // check if user already exists
+  let userExist = users.find(
+    (user) =>
+      user.id === user.id &&
+      user.nameUser === nameUser &&
+      user.roomName === roomName
+  );
+  if (userExist) {
+    return { error: "user already exists" };
+  }
+  const user = { id, nameUser, roomName };
+  users.push(user);
+  return { user };
+};
+
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id);
+  if (index !== -1) {
+    return users.splice(index, 1);
+  }
+};
+
+const getUser = (id) => users.find((user) => user.id === id);
+
+const getAllUsers = (roomName) =>
+  users.filter((user) => user.roomName === roomName);
+
+exports.modules = {
+  getAllUsers,
+  addUser,
+  removeUser,
+  getUser,
+};
